93a8bff7bc70be770125c329cea20be031714355
yydd-8888 <yd18692351472@163.com>
2020-12-05 21:13:09 +0800

    menuStrip控件

--- admin.Designer.cs
+++ admin.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.功能1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.功能2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 28);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.功能1ToolStripMenuItem,
+            this.功能2ToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip2.TabIndex = 1;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // 功能1ToolStripMenuItem
+            // 
+            this.功能1ToolStripMenuItem.Name = "功能1ToolStripMenuItem";
+            this.功能1ToolStripMenuItem.Size = new System.Drawing.Size(62, 24);
+            this.功能1ToolStripMenuItem.Text = "功能1";
+            // 
+            // 功能2ToolStripMenuItem
+            // 
+            this.功能2ToolStripMenuItem.Name = "功能2ToolStripMenuItem";
+            this.功能2ToolStripMenuItem.Size = new System.Drawing.Size(62, 24);
+            this.功能2ToolStripMenuItem.Text = "功能2";
+            // 
+            // admin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "admin";
             this.Text = "admin";
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem 功能1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 功能2ToolStripMenuItem;
     }
 }
--- customer.Designer.cs
+++ customer.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.功能1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.功能2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.功能1ToolStripMenuItem,
+            this.功能2ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 功能1ToolStripMenuItem
+            // 
+            this.功能1ToolStripMenuItem.Name = "功能1ToolStripMenuItem";
+            this.功能1ToolStripMenuItem.Size = new System.Drawing.Size(62, 24);
+            this.功能1ToolStripMenuItem.Text = "功能1";
+            // 
+            // 功能2ToolStripMenuItem
+            // 
+            this.功能2ToolStripMenuItem.Name = "功能2ToolStripMenuItem";
+            this.功能2ToolStripMenuItem.Size = new System.Drawing.Size(62, 24);
+            this.功能2ToolStripMenuItem.Text = "功能2";
+            // 
+            // customer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "customer";
             this.Text = "customer";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 功能1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 功能2ToolStripMenuItem;
     }
 }
