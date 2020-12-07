c51d261e725cb396be38fad052edf62a1562839f
weiaiwangyuan <1157368051@qq.com>
2020-12-07 20:20:11 +0800

    前台功能2-线下预定

---
+++ 前台线下预定.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 酒店管理系统
+{
+    public partial class 前台线下预定 : Form
+    {
+        public 前台线下预定()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=DESKTOP-0GCTIPK\\MSSQLSERVER1;Initial Catalog=Hotel;Integrated Security=True");
+            conn.Open();
+            String sql = "insert into order(ID,name1,id_name1,name2,id_name2,name3,id_name3,type_room,price,checkin_date,checkout_date,numberofdays,phonenumber,state)values('" + D1.Text.Trim() + "','" + G1.Text.Trim() + "','" + S1.Text.Trim() + "','" + G2.Text.Trim() + "','" + S2.Text.Trim() + "','" + G3.Text.Trim() + "','" + S3.Text.Trim() + "','" + F.Text.Trim() + "','" + M.Text.Trim() + "','" +R.Text.Trim() + "','" +T.Text.Trim() + "','" +int.Parse(D.Text.Trim()) + "','" +L.Text.Trim() + "','" +int.Parse(Z.Text.Trim()) + "')";
+            SqlCommand sqlCommand = new SqlCommand(sql, conn);
+            Z.Text = "未入住";
+            sqlCommand.ExecuteNonQuery();
+            Console.WriteLine("增加数据成功");
+            conn.Close();
+        }
+        private void label14_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            stage s = new stage();
+            s.Show();
+        }
+    }
+}
---
+++ 前台线下预定.designer.cs
@@ -0,0 +1,396 @@
+﻿
+namespace 酒店管理系统
+{
+    partial class 前台线下预定
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.D1 = new System.Windows.Forms.TextBox();
+            this.G1 = new System.Windows.Forms.TextBox();
+            this.G2 = new System.Windows.Forms.TextBox();
+            this.G3 = new System.Windows.Forms.TextBox();
+            this.L = new System.Windows.Forms.TextBox();
+            this.S1 = new System.Windows.Forms.TextBox();
+            this.S2 = new System.Windows.Forms.TextBox();
+            this.S3 = new System.Windows.Forms.TextBox();
+            this.R = new System.Windows.Forms.TextBox();
+            this.T = new System.Windows.Forms.TextBox();
+            this.D = new System.Windows.Forms.TextBox();
+            this.M = new System.Windows.Forms.TextBox();
+            this.F = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label14 = new System.Windows.Forms.Label();
+            this.Z = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "订单编号";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "顾客1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "顾客2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(54, 335);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "顾客3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(45, 441);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "联系电话";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(703, 281);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "房间类型";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(703, 361);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 15);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "入住天数 ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(703, 441);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(67, 15);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "订单金额";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(332, 118);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(82, 15);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "身份证号码";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(332, 225);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(82, 15);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "身份证号码";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(332, 335);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(82, 15);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "身份证号码";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(703, 200);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(67, 15);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "退房日期";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(703, 115);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(67, 15);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "入住日期";
+            // 
+            // D1
+            // 
+            this.D1.Location = new System.Drawing.Point(118, 34);
+            this.D1.Name = "D1";
+            this.D1.Size = new System.Drawing.Size(193, 25);
+            this.D1.TabIndex = 13;
+            this.D1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // G1
+            // 
+            this.G1.Location = new System.Drawing.Point(133, 115);
+            this.G1.Name = "G1";
+            this.G1.Size = new System.Drawing.Size(162, 25);
+            this.G1.TabIndex = 14;
+            // 
+            // G2
+            // 
+            this.G2.Location = new System.Drawing.Point(133, 222);
+            this.G2.Name = "G2";
+            this.G2.Size = new System.Drawing.Size(162, 25);
+            this.G2.TabIndex = 15;
+            // 
+            // G3
+            // 
+            this.G3.Location = new System.Drawing.Point(133, 332);
+            this.G3.Name = "G3";
+            this.G3.Size = new System.Drawing.Size(162, 25);
+            this.G3.TabIndex = 16;
+            // 
+            // L
+            // 
+            this.L.Location = new System.Drawing.Point(133, 431);
+            this.L.Name = "L";
+            this.L.Size = new System.Drawing.Size(162, 25);
+            this.L.TabIndex = 17;
+            // 
+            // S1
+            // 
+            this.S1.Location = new System.Drawing.Point(440, 115);
+            this.S1.Name = "S1";
+            this.S1.Size = new System.Drawing.Size(234, 25);
+            this.S1.TabIndex = 18;
+            // 
+            // S2
+            // 
+            this.S2.Location = new System.Drawing.Point(440, 222);
+            this.S2.Name = "S2";
+            this.S2.Size = new System.Drawing.Size(234, 25);
+            this.S2.TabIndex = 19;
+            // 
+            // S3
+            // 
+            this.S3.Location = new System.Drawing.Point(440, 332);
+            this.S3.Name = "S3";
+            this.S3.Size = new System.Drawing.Size(234, 25);
+            this.S3.TabIndex = 20;
+            // 
+            // R
+            // 
+            this.R.Location = new System.Drawing.Point(788, 115);
+            this.R.Name = "R";
+            this.R.Size = new System.Drawing.Size(234, 25);
+            this.R.TabIndex = 21;
+            // 
+            // T
+            // 
+            this.T.Location = new System.Drawing.Point(788, 197);
+            this.T.Name = "T";
+            this.T.Size = new System.Drawing.Size(234, 25);
+            this.T.TabIndex = 22;
+            // 
+            // D
+            // 
+            this.D.Location = new System.Drawing.Point(788, 358);
+            this.D.Name = "D";
+            this.D.Size = new System.Drawing.Size(234, 25);
+            this.D.TabIndex = 23;
+            // 
+            // M
+            // 
+            this.M.Location = new System.Drawing.Point(788, 438);
+            this.M.Name = "M";
+            this.M.Size = new System.Drawing.Size(234, 25);
+            this.M.TabIndex = 24;
+            // 
+            // F
+            // 
+            this.F.FormattingEnabled = true;
+            this.F.Items.AddRange(new object[] {
+            "单人间",
+            "双人间",
+            "三人间",
+            "豪华套房",
+            "蜜月套房",
+            "商务套房"});
+            this.F.Location = new System.Drawing.Point(788, 278);
+            this.F.Name = "F";
+            this.F.Size = new System.Drawing.Size(234, 23);
+            this.F.TabIndex = 25;
+            this.F.Text = "单人间";
+            this.F.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(292, 509);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 36);
+            this.button1.TabIndex = 26;
+            this.button1.Text = "预定";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(621, 509);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 36);
+            this.button2.TabIndex = 27;
+            this.button2.Text = "取消";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(347, 427);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(67, 15);
+            this.label14.TabIndex = 28;
+            this.label14.Text = "入住状态";
+            this.label14.Click += new System.EventHandler(this.label14_Click);
+            // 
+            // Z
+            // 
+            this.Z.Location = new System.Drawing.Point(440, 424);
+            this.Z.Name = "Z";
+            this.Z.Size = new System.Drawing.Size(234, 25);
+            this.Z.TabIndex = 29;
+            // 
+            // 前台线下预定
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1068, 561);
+            this.Controls.Add(this.Z);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.F);
+            this.Controls.Add(this.M);
+            this.Controls.Add(this.D);
+            this.Controls.Add(this.T);
+            this.Controls.Add(this.R);
+            this.Controls.Add(this.S3);
+            this.Controls.Add(this.S2);
+            this.Controls.Add(this.S1);
+            this.Controls.Add(this.L);
+            this.Controls.Add(this.G3);
+            this.Controls.Add(this.G2);
+            this.Controls.Add(this.G1);
+            this.Controls.Add(this.D1);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "前台线下预定";
+            this.Text = "前台线下预定";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox D1;
+        private System.Windows.Forms.TextBox G1;
+        private System.Windows.Forms.TextBox G2;
+        private System.Windows.Forms.TextBox G3;
+        private System.Windows.Forms.TextBox L;
+        private System.Windows.Forms.TextBox S1;
+        private System.Windows.Forms.TextBox S2;
+        private System.Windows.Forms.TextBox S3;
+        private System.Windows.Forms.TextBox R;
+        private System.Windows.Forms.TextBox T;
+        private System.Windows.Forms.TextBox D;
+        private System.Windows.Forms.TextBox M;
+        private System.Windows.Forms.ComboBox F;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox Z;
+    }
+}
