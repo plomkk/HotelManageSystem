966d0ff66a5a7c1b844ef52917516f3aa804ec0e
1987993976@qq.com <1987993976@qq.com>
2020-12-10 21:10:12 +0800

    CheckOut

--- CheckOut.Designer.cs
+++ CheckOut.Designer.cs
@@ -31,20 +31,21 @@ namespace 酒店管理系统
         {
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.Search = new System.Windows.Forms.Button();
-            this.Show = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(53, 48);
+            this.label1.Location = new System.Drawing.Point(40, 48);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(80, 18);
+            this.label1.Size = new System.Drawing.Size(116, 18);
             this.label1.TabIndex = 3;
-            this.label1.Text = "查询订单";
+            this.label1.Text = "请输入房间号";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // textBox1
             // 
@@ -53,44 +54,45 @@ namespace 酒店管理系统
             this.textBox1.Size = new System.Drawing.Size(281, 28);
             this.textBox1.TabIndex = 4;
             // 
-            // Search
-            // 
-            this.Search.Location = new System.Drawing.Point(483, 35);
-            this.Search.Name = "Search";
-            this.Search.Size = new System.Drawing.Size(100, 44);
-            this.Search.TabIndex = 5;
-            this.Search.Text = "查询订单";
-            this.Search.UseVisualStyleBackColor = true;
-            this.Search.Click += new System.EventHandler(this.Search_Click);
-            // 
-            // Show
-            // 
-            this.Show.Location = new System.Drawing.Point(615, 35);
-            this.Show.Name = "Show";
-            this.Show.Size = new System.Drawing.Size(127, 44);
-            this.Show.TabIndex = 6;
-            this.Show.Text = "显示全部订单";
-            this.Show.UseVisualStyleBackColor = true;
-            // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(30, 94);
+            this.dataGridView1.Location = new System.Drawing.Point(30, 114);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 30;
-            this.dataGridView1.Size = new System.Drawing.Size(744, 323);
+            this.dataGridView1.Size = new System.Drawing.Size(744, 303);
             this.dataGridView1.TabIndex = 7;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(480, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 31);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "办理退房";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(622, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 18);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "显示退房订单信息";
             // 
             // CheckOut
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.Show);
-            this.Controls.Add(this.Search);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Name = "CheckOut";
@@ -105,8 +107,8 @@ namespace 酒店管理系统
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button Search;
-        private System.Windows.Forms.Button Show;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
     }
 }
--- CheckOut.cs
+++ CheckOut.cs
@@ -18,37 +18,7 @@ namespace 酒店管理系统
             InitializeComponent();
         }
 
-        private void Search_Click(object sender, EventArgs e)
-        {
-            SqlConnection conn = new SqlConnection("Data Source=DESKTOP-RD7CTBN;Initial Catalog=Hotel;Integrated Security=True");
-            conn.Open();
-            String strSQL = "Select* from [order] where [id]='" + textBox1.Text.Trim() + "'";
-            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(strSQL, conn);
-            DataSet dt = new DataSet();
-            sqlDataAdapter.Fill(dt);
-            dataGridView1.DataSource = dt.Tables[0];
-            DataGridViewButtonColumn btn = new DataGridViewButtonColumn();
-            btn.Name = "btnModify";
-            btn.HeaderText = "退住修改";
-            btn.DefaultCellStyle.NullValue = "退住";
-            dataGridView1.Columns[0].HeaderText = "订单编号";
-            dataGridView1.Columns[1].HeaderText = "顾客1";
-            dataGridView1.Columns[2].HeaderText = "身份证号1";
-            dataGridView1.Columns[3].HeaderText = "顾客2";
-            dataGridView1.Columns[4].HeaderText = "身份证号2";
-            dataGridView1.Columns[5].HeaderText = "顾客3";
-            dataGridView1.Columns[6].HeaderText = "身份证号3";
-            dataGridView1.Columns[7].HeaderText = "房间号";
-            dataGridView1.Columns[8].HeaderText = "房间类型";
-            dataGridView1.Columns[9].HeaderText = "订单价格";
-            dataGridView1.Columns[10].HeaderText = "入住时间";
-            dataGridView1.Columns[11].HeaderText = "退房时间";
-            dataGridView1.Columns[12].HeaderText = "入住天数";
-            dataGridView1.Columns[13].HeaderText = "联系方式";
-            dataGridView1.Columns[14].HeaderText = "订单状态";
-            dataGridView1.Columns.Add(btn);
-        }
-
+      
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             if (dataGridView1.Columns[e.ColumnIndex].Name == "btnModify")
@@ -69,6 +39,46 @@ namespace 酒店管理系统
                     Console.WriteLine("更新记录操作失败");
                 }
             }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=DESKTOP-RD7CTBN;Initial Catalog=Hotel;Integrated Security=True");
+            conn.Open();
+            String sql1 = "update [order] set [state]='已入住' where [id_room]="+textBox1.Text.Trim();
+            String sql2= "update [room] set [state]='可用'where [ID]=" + textBox1.Text.Trim();
+            SqlCommand cmd1 = new SqlCommand(sql1, conn);
+            SqlCommand cmd2 = new SqlCommand(sql2, conn);
+            cmd1.CommandText = sql1;
+            cmd2.CommandText = sql2;
+            cmd1.ExecuteNonQuery();
+            cmd2.ExecuteNonQuery();
+            MessageBox.Show("退房办理成功");
+            String strSQL = "Select * from [order] where [id_room]=" + textBox1.Text.Trim();
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(strSQL, conn);
+            DataSet dt = new DataSet();
+            sqlDataAdapter.Fill(dt);
+            dataGridView1.DataSource = dt.Tables[0];
+            dataGridView1.Columns[0].HeaderText = "订单编号";
+            dataGridView1.Columns[1].HeaderText = "顾客1";
+            dataGridView1.Columns[2].HeaderText = "身份证号1";
+            dataGridView1.Columns[3].HeaderText = "顾客2";
+            dataGridView1.Columns[4].HeaderText = "身份证号2";
+            dataGridView1.Columns[5].HeaderText = "顾客3";
+            dataGridView1.Columns[6].HeaderText = "身份证号3";
+            dataGridView1.Columns[7].HeaderText = "房间号";
+            dataGridView1.Columns[8].HeaderText = "房间类型";
+            dataGridView1.Columns[9].HeaderText = "订单价格";
+            dataGridView1.Columns[10].HeaderText = "入住时间";
+            dataGridView1.Columns[11].HeaderText = "退房时间";
+            dataGridView1.Columns[12].HeaderText = "入住天数";
+            dataGridView1.Columns[13].HeaderText = "联系方式";
+            dataGridView1.Columns[14].HeaderText = "订单状态";
         }
     }
 }
